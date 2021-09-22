7f94184e35ce5bd9099ea9ff90304ffc541b11cf
Anonymous user <85908826+j5rlLqM-rvsrY96V-GpVJP0F-IZFesRzy-ShMoz@users.noreply.github.com>
2021-09-22 03:02:25 +0000

    Cleanup

--- Mahoujin/Builder/build.gd
+++ Mahoujin/Builder/build.gd
@@ -1,6 +1,5 @@
 extends "res://cuberotations.gd"
-
-var fle : File = File.new()
+var file : File = File.new()
 
 
 func get_meshinstances(n : Node):
@@ -29,32 +28,10 @@ func add_part(part : int, pos : Vector3, rot : int, cols_only : bool = false):
 	if cols_only:	name = "res://build_colls/"+name
 	else:			name = "res://build_parts/"+name
 	
-	if fle.file_exists(name):
+	if file.file_exists(name):
 		var p : Spatial = ResourceLoader.load(name, "PackedScene").instance()
 		p.transform.origin = pos/5
 		p.transform.basis = Basis(dir[rot])
 		return p
 	else:
-		print("Builder: No such file: ", name)
-
-
-
-#func _ready():
-#	var bot = $Parser.interpret("res://2plates.bot")
-#	#2plates.bot
-#	#rod3Ddiagonal.bot
-#	#strut3Ddiagonalshort.bot
-#	#bot.json
-#	#Fijanub.bot
-#	#FijaNub.bot
-#	#FijaNoob.bot
-#	#$Triforcer.clean_macro(bot)
-#	var re = $"../Triforcer".tryforce(bot, [[3365106304, 3001609712, 2]])
-##	var bot : Array = []
-##	for i in len(chassis):
-##		var rot = 0
-##		bot.append([ chassis.keys()[i], Vector3(10*i, 0, 0), rot])
-##		for c in $Triforcer.get_c_points(chassis.keys()[i], Vector3(10*i, 0, rot)):
-##			bot.append([1931676396, c[1]+c[0], 0])
-#	_build(bot)
-#	$User/Camera.set_current(true)
+		push_warning("Builder: No such file: "+name)
